6122e9191d222d91ab6173570747377ba9fb3efc
MaxSac <maximilian.sackel@gmx.de>
2018-04-16 14:26:51 +0200

    correct task 1a

--- 00/src/roundings.cpp
+++ 00/src/roundings.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <cmath>
 
-double a1(long i){
+double a1(double i){
 		return 1/sqrt(i) - 1/sqrt(i + 1);
 }
 
-double a2(long i){
-		return 1/sqrt(i*(i + 1))*(sqrt(i + 1)*sqrt(i));
+double a2(double i){
+		return 1/(sqrt(i*(i + 1))*(sqrt(i + 1)*sqrt(i)));
 }
+
 int main(){
-		for(long i=1; i<pow(10, 12); i=i*10){
-			printf("%i %f %f\n", i, a1(i), a2(i));
+		printf("Task 1: \n");
+		printf("a) \n");
+		for(long long int i = 1; i<pow(10, 12); i=i*10){
+			printf("%lld %f %f\n", i, a1(i), a2(i));
 		}
 		return 0;
 }
