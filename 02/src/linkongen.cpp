b9b1f7f8c0698aefe49ee9e1a444f39818f3090b
Jan Spinne <spinne@dynip-129-217-070-183.wifi.tu-dortmund.de>
2018-04-26 15:35:53 +0200

    blatt 2, linkongen added

---
+++ 02/src/linkongen.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+# include <cstdio>
+# include <cmath>
+# include <random>
+# include <fstream>
+# include <array>
+
+using namespace std;
+
+
+void linkongen(int32_t r_0, int32_t a, int32_t c, int32_t m, int32_t n, string file_name){
+
+	ofstream storage;
+  	storage.open (file_name.c_str());
+  	storage << "Writing this to a file.\n";
+  	storage.precision(10);
+
+
+	int32_t r = r_0;
+	for(i=0; i<n; i++){
+		storage << (double) r/m << endl;
+		r=(a*r+c)%m;
+	}
+}
+
+
+int main(){
+	int64_t N=pow(10,5);
+	cout << N << endl;
+	linkongen(1234, 20, 120, 6075, N, "a_1_a.txt")
+
+
+}
