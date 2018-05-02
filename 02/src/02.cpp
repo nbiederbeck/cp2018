1372318818d852c77a6365503a680f03fa5e39f8
MaxSac <maximilian.sackel@gmx.de>
2018-05-02 17:39:03 +0200

    box muller algo

--- 02/src/02.cpp
+++ 02/src/02.cpp
@@ -15,12 +15,12 @@ void a() {
 		uniform_real_distribution<> dist(0, 1);
 
 		vector<double> randcos;
-        double x;
+		double x;
 		for(int i = 0; i<pow(10,6); i++){
 				x = dist(rng);
 				randcos.push_back(asin(x));
 		}
-		
+
 		uniform_real_distribution<> Dist(-1, 1);
 		vector<double> Randcos;
 		for(int i = 0; i<pow(10,6); i++){
@@ -35,9 +35,38 @@ void a() {
 				}
 				myfile.close();
 		}
-		
+
 }
 
+void b(float mu, float sigma2){
+		mt19937 rng;
+		rng.seed(random_device()());
+		uniform_real_distribution<> dist(0, 1);
+
+		double U_1 = 0.;
+		double U_2 = 0.;
+		double Z_1 = 0.;
+		double Z_2 = 0.;
+
+		ofstream myfile ("./build/task2b.txt");
+		if (myfile.is_open()) {
+				for (int i=0; i<pow(10,5); i++){
+						U_1 = dist(rng);
+						U_2 = dist(rng);
+
+						Z_1 = sqrt(-2.*log(U_1))*cos(2*M_PI*U_2);
+						Z_2 = sqrt(-2.*log(U_1))*sin(2*M_PI*U_2);
+
+						Z_1 = mu + Z_1 * sigma2;
+						Z_2 = mu + Z_2 * sigma2;
+						myfile << Z_1 << ";" << Z_2 << endl;
+				}
+		}
+}
+
+
+
 int main() {
-		a();
+		/* a(); */
+		b(3., 4.);
 }
