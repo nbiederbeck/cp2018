ac8dbe087bad2cc5ecf208a0b43d17becb17463c
MaxSac <maximilian.sackel@gmx.de>
2018-05-15 23:14:34 +0200

    add periodical

--- 03/src/02.cpp
+++ 03/src/02.cpp
@@ -10,27 +10,44 @@ int** init_lattice(int m, int n)
 		std::mt19937 rng;
 		rng.seed(std::random_device()());
 		std::discrete_distribution<> random_choice({1, 1});
-
-		int **p2DArray;
-		p2DArray = new int*[m+2];
+		
+		// init n+2xm+2 array
+		int **lattice;
+		lattice = new int*[m+2];
 		for (int i = 0; i < m+2; ++i)
-				p2DArray[i] = new int[n+2];
+				lattice[i] = new int[n+2];
 
 		// Assign values
 		for(int x=1; x<m+1 ; x++){
 				for(int y=1; y<n+1 ; y++){
-						p2DArray[x][y] = random_choice(rng);
+						lattice[x][y] = random_choice(rng);
 				}
 		}
-
-		return p2DArray;
+		return lattice;
 }
+
+int** periodic_lattice(int** lattice, int m, int n){
+		for(int i=1; i<m+1; i++){
+				lattice[0][i] = lattice[m][i];
+				lattice[m+1][i] = lattice[1][i];
+		}
+		for(int i=1; i<n+1; i++){
+				lattice[i][0] = lattice[i][n];
+				lattice[i][n+1] = lattice[i][1];
+		}
+}
+
 int main(int argc, char *argv[])
 {
-		int m = 2;
-		int n=2;
+		int m = 5;
+		int n = 5;
+
+		// init random lattice
 		int** rvalue;
 		rvalue = init_lattice(m,n);
+
+		// make periodical boundary conditions
+		periodic_lattice(rvalue, m, n);
 
 		// print lattice 
 		for(int x=0; x<m+2 ; x++){
