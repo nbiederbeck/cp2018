bcb4b8d8887196f158432a6d2d5eb259ddb76af6
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-05-17 11:57:47 +0200

    add plotting scripts for lattices and energies

--- 03/src/plot_lattice.py
+++ 03/src/plot_lattice.py
@@ -1,40 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-init_file = "build/02_init_ran_0_kbT_3.txt"
-final_file = "build/02_sweep_ran_0_kbT_3.txt"
-
-init = np.genfromtxt(init_file, delimiter=",")
-n, m = init.shape
-init = init[1:n - 1, 1:m - 2]
-
-final = np.genfromtxt(final_file, delimiter=",")
-n, m = final.shape
-final = final[1:n - 1, 1:m - 2]
+ran = [0, 1]
+kbT = [1, 2.27, 3]
+snapshots = ["init", "sweep"]
 
 
-fig = plt.figure()
-fig.set_size_inches(10, 4)
+def main():
+    for r in ran:
+        for k in kbT:
+            init = "build/02_init_ran_{r}_kbT_{k}.txt".format(r=r, k=k)
+            final = "build/02_sweep_ran_{r}_kbT_{k}.txt".format(r=r, k=k)
+            plot(init, final, r, k)
 
-ax_first = fig.add_subplot(121)
-ax_final = fig.add_subplot(122)
 
-cmap = plt.get_cmap("binary", 2)
+def plot(init_file, final_file, ran, kbT):
+    if kbT == 2.27:
+        kbT *= 100
+    filename = "build/lattice_ran_{r}_kbT_{k:.0f}.png".format(r=ran, k=kbT)
+    if kbT == 227:
+        kbT /= 100
+    print("plot {}".format(filename))
+    init = np.genfromtxt(init_file, delimiter=",")
+    n, m = init.shape
+    init = init[1:n - 1, 1:m - 2]
 
-im_first = ax_first.imshow(init, cmap=cmap)
-ax_first.set_title("Randomly initlialized lattice.")
-ax_first.set_xlabel("x")
-ax_first.set_ylabel("y")
+    final = np.genfromtxt(final_file, delimiter=",")
+    n, m = final.shape
+    final = final[1:n - 1, 1:m - 2]
 
-im_final = ax_final.imshow(final, cmap=cmap)
-ax_final.set_title("Lattice after MC.")
-ax_final.set_xlabel("x")
-ax_final.set_ylabel("y")
+    fig = plt.figure()
+    fig.set_size_inches(9.5, 4)
 
-cbar = fig.colorbar(im_first)
-cbar.set_ticks([-1, 1])
-cbar.ax.set_ylabel("Spin")
+    ax_first = fig.add_subplot(121)
+    ax_final = fig.add_subplot(122)
 
-fig.tight_layout(pad=0)
-fig.savefig("build/cpp_sweep.png")
+    cmap = plt.get_cmap("binary", 2)
+
+    im_first = ax_first.imshow(init, cmap=cmap)
+    ax_first.set_title("Initialized Lattice.", fontsize=10)
+    ax_first.set_xlabel("x")
+    ax_first.set_ylabel("y")
+
+    im_final = ax_final.imshow(final, cmap=cmap)
+    ax_final.set_title("Lattice After MC.", fontsize=10)
+    ax_final.set_xlabel("x")
+    ax_final.set_ylabel("y")
+
+    cbar = fig.colorbar(im_first)
+    cbar.set_ticks([-1, 1])
+    cbar.ax.set_ylabel("Spin")
+
+    fig.suptitle(r"ISING MC, $k_BT=${k}".format(k=kbT), fontsize=16)
+    fig.tight_layout()
+    fig.savefig(filename)
+
+
+if __name__ == "__main__":
+    main()
