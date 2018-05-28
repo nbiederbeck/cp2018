03a99e3ba3bc0bccf9e0c507456fa607edc9d1aa
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-05-28 18:06:30 +0200

    run verbose

--- 05/src/01.cpp
+++ 05/src/01.cpp
@@ -1,4 +1,12 @@
+// Aufgabe 1: Diagonalisierung per Hand
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
+    cout << "====================================" << endl;
+    cout << "Aufgabe 1: Diagonalisierung per Hand" << endl;
+    cout << "====================================" << endl << endl;
     return 0;
 }
--- 05/src/02.cpp
+++ 05/src/02.cpp
@@ -1,4 +1,12 @@
+// Aufgabe 2: 1D Heisenbergkette
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
+    cout << "=============================" << endl;
+    cout << "Aufgabe 2: 1D Heisenbergkette" << endl;
+    cout << "=============================" << endl << endl;
     return 0;
 }
