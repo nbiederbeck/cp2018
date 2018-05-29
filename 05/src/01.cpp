735aa30cdcdf1fcf6b20ef7b880c92a11c7f6a70
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-05-29 21:16:19 +0200

    Add cwiseProduct to while loop

--- 05/src/01.cpp
+++ 05/src/01.cpp
@@ -18,7 +18,7 @@ using Eigen::MatrixXcd;
 using Eigen::VectorXd;
 
 void eigen_solve(MatrixXd M);
-void save_eigenvalues(MatrixXcd ev, const char *filename);
+void save_matrices_to_file(MatrixXcd ev, const char *filename);
 
 MatrixXd householder(MatrixXd M) {
     cout << "Householder Algorithm: " << endl;
@@ -27,29 +27,29 @@ MatrixXd householder(MatrixXd M) {
     VectorXd a;
     double alpha;
     double r;
-    int dim = M.rows();
+    const int dim = M.rows();
     VectorXd v(dim);
 
 
     // Folge Wikipedia Anleitung:
     // https://en.wikipedia.org/wiki/Householder_transformation#Examples
-    for(int a_row=0; a_row<dim-2; a_row++) {
+    for(int a_row = 0; a_row < dim - 2; a_row++) {
         a = M.row(a_row);
         alpha = -1.0 * copysign(a.tail(dim - a_row - 1).norm(), a(a_row + 1));
-        r = std::sqrt(0.5 * (std::pow(alpha, 2)-a(a_row + 1)*alpha));
+        r = std::sqrt(0.5 * (std::pow(alpha, 2) - a(a_row + 1) * alpha));
 
         v(a_row) = 0.;
         v(a_row + 1) = (a(a_row + 1) - alpha) / (2 * r);
 
-        for(int i=a_row+2; i<dim; i++) {
-            v(i) = a(i)/(2*r);
+        for(int i = a_row + 2; i < dim; i++) {
+            v(i) = a(i) / (2 * r);
         }
 
         MatrixXd P = MatrixXd::Zero(dim, dim);
         P.diagonal() = MatrixXd::Ones(dim, 1);
 
-        P -= 2*v*v.transpose();
-        M = P.transpose()*M*P;
+        P -= 2 * v * v.transpose();
+        M = P.transpose() * M * P;
 
         for (int i = 0; i < dim; ++i) {
             for (int j = 0; j < dim; ++j) {
@@ -58,8 +58,6 @@ MatrixXd householder(MatrixXd M) {
                 }
             }
         }
-
-
     }
 
     cout << "Tridiagonalematrix nach Householder-Algorithmus:" << endl;
@@ -70,42 +68,42 @@ MatrixXd householder(MatrixXd M) {
 
 MatrixXd jacobi_rotation(MatrixXd M) {
     const int dim = M.rows();
-    double eps = 1.0;
+    const double eps = 1e-0;
     int q;
     double theta, t, c, s;
-    // use sum(a_ij^2)
-    // while (((M).sum() - (M).diagonal().sum()) > eps) {
-    for (int p = 0; p < dim - 1; ++p) {
-        q = p + 1;
-        theta = (M(q, q) - M(p, p)) / (2.0 * M(p, q));
+    int jacobicounter = 0;
 
-        t = copysign(1.0, theta) / (abs(theta) + sqrt(pow(theta, 2.0) + 1.0));
-        c = 1.0 / sqrt(1.0 + pow(t, 2.0));
-        s = t * c;
+    while (((M.cwiseProduct(M)).sum() - (M.cwiseProduct(M)).trace()) > eps) {
+        ++jacobicounter;
+        for (int p = 0; p < dim - 1; ++p) {
+            q = p + 1;
+            theta = (M(q, q) - M(p, p)) / (2.0 * M(p, q));
 
-        MatrixXd P = MatrixXd::Zero(dim, dim);
-        P.diagonal() = MatrixXd::Ones(dim, 1);
-        P(p, p) = c;
-        P(q, q) = c;
-        P(p, q) = s;
-        P(q, p) = -s;
+            t = copysign(1.0, theta) / (abs(theta) + sqrt(pow(theta, 2.0) + 1.0));
+            c = 1.0 / sqrt(1.0 + pow(t, 2.0));
+            s = t * c;
 
-        std::cout << p << std::endl;
+            MatrixXd P = MatrixXd::Zero(dim, dim);
+            P.diagonal() = MatrixXd::Ones(dim, 1);
+            P(p, p) = c;
+            P(q, q) = c;
+            P(p, q) = s;
+            P(q, p) = -s;
 
-        M = P.transpose() * M * P;
+            M = P.transpose() * M * P;
 
-        for (int i = 0; i < dim; ++i) {
-            for (int j = 0; j < dim; ++j) {
-                if (abs(M(i, j)) < 1e-8) {
-                    M(i, j) = 0.0;
+            for (int i = 0; i < dim; ++i) {
+                for (int j = 0; j < dim; ++j) {
+                    if (abs(M(i, j)) < 1e-8) {
+                        M(i, j) = 0.0;
+                    }
                 }
             }
+            std::cout << M << std::endl;
         }
-
-        std::cout << M << std::endl;
-        // }
-}
-return M;
+    }
+    cout << "# of Jacobi-Rotations ('Sweeps'): " << jacobicounter << endl;
+    return M;
 }
 
 int main(int argc, char *argv[])
@@ -134,11 +132,11 @@ int main(int argc, char *argv[])
 
     // Aufgabenteil (a)
     M = householder(M);
-    save_eigenvalues(M, "build/householder.txt");
+    save_matrices_to_file(M, "build/householder.txt");
 
     M = jacobi_rotation(M);
 
-    save_eigenvalues(M.diagonal(), "build/hand_eigenvalues.txt");
+    save_matrices_to_file(M.diagonal(), "build/hand_eigenvalues.txt");
 
     return 0;
 }
@@ -152,10 +150,10 @@ void eigen_solve(MatrixXd M) {
     cout << "Eigenwerte der Matrix M, geloest durch Eigen::EigenSolver" << endl;
     cout << ev << endl;
 
-    save_eigenvalues(ev, "build/eigensolver_eigenvalues.txt");
+    save_matrices_to_file(ev, "build/eigensolver_eigenvalues.txt");
 }
 
-void save_eigenvalues(MatrixXcd ev, const char *filename) {
+void save_matrices_to_file(MatrixXcd ev, const char *filename) {
     std::ofstream file (filename);
     file << ev << endl;
 }
