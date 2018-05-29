ac806cbadf1020a3e715ddb905c02c12e431de67
MaxSac <maximilian.sackel@gmx.de>
2018-05-29 16:32:58 +0200

    vkalsknlakngv;lakjnklsjanv

--- 05/src/01.cpp
+++ 05/src/01.cpp
@@ -7,51 +7,105 @@
 
 using std::cout;
 using std::endl;
-using std::signbit;
+using std::copysign;
+using std::pow;
+using std::sqrt;
 
-using namespace Eigen;
+// using namespace Eigen;
 using Eigen::EigenSolver;
 using Eigen::MatrixXd;
+using Eigen::MatrixXcd;
 using Eigen::VectorXd;
 
 void eigen_solve(MatrixXd M);
 void save_eigenvalues(MatrixXcd ev, const char *filename);
-void householder(MatrixXd M) {
+
+MatrixXd householder(MatrixXd M) {
 		cout << "Householder Algorithm: " << endl;
 		cout << "====================================" << endl << endl;
 
 		VectorXd a;
-		int act_row; double alpha;
+		double alpha;
 		double r;
 		int dim = M.rows();
 		VectorXd v(dim);
 
-		MatrixXd P(dim, dim);
 
-		act_row = 0;
-		a = M.row(act_row);
-		if(signbit(a(act_row+1)) != 0){
-				alpha = std::sqrt(a.tail(dim-act_row-1).squaredNorm());
-		}
-		else{
-				alpha = -1.0 * sqrt(a.tail(dim-act_row-1).squaredNorm());
-		};
-		r = std::sqrt(0.5*(std::pow(alpha, 2)-a(act_row+1)*alpha));
-		v(0) = 0.;
-		v(1) = (a(act_row+1) - alpha)/(2*r);
-		for(int i=2; i<dim; i++){
-				v(i) = a(i)/(2*r);
-		}
-		
-		P.diagonal() = MatrixXd::Ones(dim, 1);
-		P -= 2*v*v.transpose();
-		M = P.transpose()*M*P;
+		// Folge Wikipedia Anleitung:
+		// https://en.wikipedia.org/wiki/Householder_transformation#Examples
+		for(int a_row=0; a_row<dim-2; a_row++) {
+				a = M.row(a_row);
+				alpha = -1.0 * copysign(a.tail(dim - a_row - 1).norm(), a(a_row + 1));
+				r = std::sqrt(0.5 * (std::pow(alpha, 2)-a(a_row + 1)*alpha));
 
-		std::cout <<  "alpha:" << alpha << std::endl;
-		std::cout <<  "r:" << r << std::endl;
-		std::cout <<  "v:" << v << std::endl;
-		std::cout <<  "P:" << P << std::endl;
-		std::cout <<  "M:" << M << std::endl;
+				v(a_row) = 0.;
+				v(a_row + 1) = (a(a_row + 1) - alpha) / (2 * r);
+
+				for(int i=a_row+2; i<dim; i++) {
+						v(i) = a(i)/(2*r);
+				}
+
+				MatrixXd P = MatrixXd::Zero(dim, dim);
+				P.diagonal() = MatrixXd::Ones(dim, 1);
+
+				P -= 2*v*v.transpose();
+				M = P.transpose()*M*P;
+
+				for (int i = 0; i < dim; ++i) {
+						for (int j = 0; j < dim; ++j) {
+								if (abs(M(i, j)) < 1e-8) {
+										M(i, j) = 0;
+								}
+						}
+				}
+
+
+		}
+
+		cout << "Tridiagonalematrix nach Householder-Algorithmus:" << endl;
+
+		cout << M << endl;
+		return M;
+}
+
+MatrixXd jacobi_rotation(MatrixXd M) {
+		const int dim = M.rows();
+		double eps = 1.0;
+		int q;
+		double theta, t, c, s;
+		// use sum(a_ij^2)
+		// while (((M).sum() - (M).diagonal().sum()) > eps) {
+				for (int p = 0; p < dim - 1; ++p) {
+						q = p + 1;
+						theta = (M(q, q) - M(p, p)) / (2.0 * M(p, q));
+
+						t = copysign(1.0, theta) / (abs(theta) + sqrt(pow(theta, 2.0) + 1.0));
+						c = 1.0 / sqrt(1.0 + pow(t, 2.0));
+						s = t * c;
+
+						MatrixXd P = MatrixXd::Zero(dim, dim);
+						P.diagonal() = MatrixXd::Ones(dim, 1);
+						P(p, p) = c;
+						P(q, q) = c;
+						P(p, q) = s;
+						P(q, p) = -s;
+
+						std::cout << p << std::endl;
+
+						M = P.transpose() * M * P;
+
+						for (int i = 0; i < dim; ++i) {
+								for (int j = 0; j < dim; ++j) {
+										if (abs(M(i, j)) < 1e-8) {
+												M(i, j) = 0.0;
+										}
+								}
+						}
+
+						std::cout << M << std::endl;
+				// }
+		}
+		return M;
 }
 
 int main(int argc, char *argv[])
@@ -71,9 +125,11 @@ int main(int argc, char *argv[])
 		// M.diagonal() = d;
 		cout << M << endl;
 
-		householder(M);
-
 		eigen_solve(M);
+
+		M = householder(M);
+
+		M = jacobi_rotation(M);
 
 		return 0;
 }
