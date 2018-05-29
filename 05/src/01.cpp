351a928ccdb8ba5a852ff58d8d7a647873a17793
MaxSac <maximilian.sackel@gmx.de>
2018-05-29 11:23:45 +0200

    fancy diag

--- 05/src/01.cpp
+++ 05/src/01.cpp
@@ -4,6 +4,7 @@
 
 using std::cout;
 using std::endl;
+using Eigen::MatrixXd;
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +12,15 @@ int main(int argc, char *argv[])
     cout << "Aufgabe 1: Diagonalisierung per Hand" << endl;
     cout << "====================================" << endl << endl;
 
-	MatrixXd m(n,n);
+	int N = 3;
+	MatrixXd m = MatrixXd::Constant(N, N, 1.0);
+	MatrixXd d(N,1);
+	d << 1, 2, 3;
+			/* = MatrixXd::Constant(N, 1, 1.0); */
+
+	m.diagonal() = d;
+
+	/* m = Eigen::MatrixXd::One(N,N); */
+    cout << m <<endl ;
     return 0;
 }
