d306ec902d607e4dde1afcfd10f76a4ef1ba84f0
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-05-30 11:46:59 +0200

    add spinzustand and permutation functions, add test function

--- 05/src/02.cpp
+++ 05/src/02.cpp
@@ -1,3 +1,4 @@
+// vim: commentstring=//%s
 // Aufgabe 2: 1D Heisenbergkette
 #include <iostream>
 #include <Eigen/Dense>
@@ -5,10 +6,55 @@
 using std::cout;
 using std::endl;
 
-int main(int argc, char *argv[])
+using Eigen::VectorXi;
+
+int spinzustand(VectorXi);
+VectorXi permutation(VectorXi, int, int);
+
+void test();
+
+int main()
 {
     cout << "=============================" << endl;
     cout << "Aufgabe 2: 1D Heisenbergkette" << endl;
     cout << "=============================" << endl << endl;
+
+    test();
+
     return 0;
+}
+
+int spinzustand(VectorXi vec) {
+    int sum = 0;
+    for (int i = 0; i < vec.rows(); i++) {
+        sum += vec(i) * pow(2, vec.rows() - i - 1);
+    }
+    return sum;
+}
+
+VectorXi permutation(VectorXi vec, int i, int j) {
+    int temp = vec(i);
+    vec(i) = vec(j);
+    vec(j) = temp;
+    return vec;
+}
+
+void test() {
+    VectorXi v(4);
+    int s;
+
+    v << 1, 0, 1, 0;
+    cout << "Vektor:" << endl << v << endl;
+
+    s = spinzustand(v);
+    cout << "Spinzustandsdarstellung: |" << s << ">" << endl;
+
+    int i, j;
+    i = 0;
+    j = 3;
+    v = permutation(v, i, j);
+    cout << "Vektor nach Permutation " << i << ", " << j << ": " << endl << v << endl;
+
+    s = spinzustand(v);
+    cout << "Spinzustandsdarstellung: |" << s << ">" << endl;
 }
