a8407846821d16b05474e666bffaad54d174ea82
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-06-04 15:25:28 +0200

    Add aufgabe 2a, sorry maxi

--- 05/src/02.cpp
+++ 05/src/02.cpp
@@ -1,6 +1,7 @@
-// vim: commentstring=//%s
+// vim: commentstring=//%s autoindent tabstop=4 shiftwidth=4 softtabstop=4 expandtab
 // Aufgabe 2: 1D Heisenbergkette
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <Eigen/Dense>
 
@@ -12,118 +13,149 @@ using std::vector;
 using Eigen::VectorXi;
 using Eigen::MatrixXi;
 
-int spinzustand(VectorXi);
+MatrixXi init_zustaende(int); // Erstelle Alle Spins in Binaerschreibweise
+MatrixXi spin_sum(MatrixXi m , int spin); // Berechne Spinsumme der Zustaende
+int zustandssumme(VectorXi);
 VectorXi permutation(VectorXi, int);
-
-MatrixXi init_zustaende(int N){
-		int dim = (int)pow(2,N);
-		MatrixXi mat(dim, N);
-		for(int d=0; d<dim; d++){
-				for(int n=0; n<N; n++){
-						mat(d, N-n-1) = d/(int)pow(2,n)%2;
-				}
-		}
-		return mat;
-}
-
-MatrixXi spin_sum(MatrixXi m , int spin){
-		// matrix with spins with corresponding spin
-		// bias because spin 0 is value 0 and not 0.5
-		double bias = m.cols()/2;
-		cout << "Bias: " << bias << " have to be checked! " << endl;
-
-		VectorXi m_spin = m.rowwise().sum().array()-(int)bias;
-		vector<int> z;
-		for(int i=0; i<m_spin.rows(); i++){
-				if(m_spin(i) == spin){
-						z.push_back(i);
-				}
-		}
-		MatrixXi spins(z.size(), m.cols());
-		for(int i=0; i<z.size(); i++){
-						spins.row(i) = m.row(z[i]);
-		}
-
-		return spins;
-}
-
+MatrixXi hamilton_matrix_from_spins(MatrixXi);
+int spin_encoder(MatrixXi, int);
 void test();
+void save_matrices_to_file(MatrixXi, const char *filename);
 
-int main()
-{
-		cout << "=============================" << endl;
-		cout << "Aufgabe 2: 1D Heisenbergkette" << endl;
-		cout << "=============================" << endl << endl;
+int main() {
+    cout << "=============================" << endl;
+    cout << "Aufgabe 2: 1D Heisenbergkette" << endl;
+    cout << "=============================" << endl << endl;
 
-		int N = 4;
-		MatrixXi spins;
-		spins = init_zustaende(N);
-		spins = spin_sum(spins, 0);
-		for(int i = 0; i<spins.rows(); i++ ){
-				cout << " |" << spinzustand(spins.row(i)) << ">" << endl;
-		}
+    int N = 10;
+    MatrixXi spins;
+    spins = init_zustaende(N);
+    spins = spin_sum(spins, 0);
 
-		double E = 0;
-		for (int i = 0; i < N; i++) {
-				E += 2 * spinzustand(permutation(spins.row(0), i));
-		}
-		E -= (double)N * spinzustand(spins.row(0));
-		std::cout << E << std::endl;
-		
-		// test();
+    MatrixXi H;
+    H = hamilton_matrix_from_spins(spins);
 
-		return 0;
+    save_matrices_to_file(H, "build/hamiltonian.txt");
+
+    return 0;
 }
 
-MatrixXi hamilton_matrix_from_spins(MatrixXi spins) {
-		int dim = spins.rows();
-		// MatrixXi H(dim, dim) ;
-		H = MatrixXi::Zero(dim, dim);
-
-		// An welcher Stelle in der Hamiltonmatrix steht der permutierte Vektor
-		// (spin_encoder) ?
-		// An dieser Stelle die Hamiltonmatrix um den entsprechenden Faktor erhoehen
-		// Dafuer jede Permutation von jedem Spinvektor berechnen, um die
-		// entsprechenden Faktoren zu erhalten.
-
-		// for (int i = 0; i < dim; i++) {
-		// 		for (int j = 0; j < spins.cols(); j++) {
-		// 				H(x, y) += spin_encoder(permutation(spins.row(i), j));
-		// 		}
-		// }
-}
-
-int spinzustand(VectorXi vec) {
-		int sum = 0;
-		for (int i = 0; i < vec.rows(); i++) {
-				sum += vec(i) * pow(2, vec.rows() - i - 1);
-		}
-		return sum;
-}
-
-VectorXi permutation(VectorXi vec, int i) {
-		int N = vec.rows();
-		int j = (N + i + 1 ) % N;
-		int temp = vec(i);
-		vec(i) = vec(j);
-		vec(j) = temp;
-		return vec;
-}
-
+// Eine Testfunktion
 void test() {
-		VectorXi v(4);
-		int s;
+    int N = 4;
+    VectorXi v(N);
+    VectorXi v_perm(N);
+    int s;
 
-		v << 1, 0, 1, 0;
-		cout << "Vektor:" << endl << v << endl;
+    v << 1, 0, 1, 0;
+    cout << "Vektor:" << endl << v << endl;
 
-		s = spinzustand(v);
-		cout << "Spinzustandsdarstellung: |" << s << ">" << endl;
+    s = zustandssumme(v);
+    cout << "Spinzustandsdarstellung: |" << s << ">" << endl;
 
-		int i = 3;
-		v = permutation(v, i);
-		cout << "Vektor nach Permutation " << i << ", " << i+1 << ": " << endl << v << endl;
+    int i = 0;
+    for (i = 0; i<N; i++) {
+        v_perm = permutation(v, i);
+        cout << "Vektor nach Permutation " << i << ", " << i+1 << ": " << endl << v_perm << endl;
+        s = zustandssumme(v_perm);
+        cout << "Spinzustandsdarstellung: |" << s << ">" << endl;
+    }
+}
 
-		s = spinzustand(v);
-		cout << "Spinzustandsdarstellung: |" << s << ">" << endl;
+// Erstelle Alle Spins in Binaerschreibweise
+MatrixXi init_zustaende(int N) {
+    int dim = (int)pow(2,N);
+    MatrixXi mat(dim, N);
+    for(int d = 0; d < dim; d++) {
+        for(int n = 0; n < N; n++) {
+            mat(d, N - n - 1) = d / (int)pow(2, n) % 2;
+        }
+    }
+    return mat;
+}
+
+// Berechne Spinsumme der Zustaende
+MatrixXi spin_sum(MatrixXi m , int spin) {
+    // Spins have value 0 or 1 and not +-1/2
+    double bias = m.cols() / 2;
+    VectorXi m_spin = m.rowwise().sum().array() - (int)bias;
+
+    vector<int> z;
+    for(int i = 0; i < m_spin.rows(); i++) {
+        if(m_spin(i) == spin){
+            z.push_back(i);
+        }
+    }
+    MatrixXi spinsums(z.size(), m.cols());
+    for(int i = 0; i < z.size(); i++) {
+        spinsums.row(i) = m.row(z[i]);
+    }
+
+    return spinsums;
+}
+
+// Berechne die Hamiltonmatrix aus gegebenen Spins
+MatrixXi hamilton_matrix_from_spins(MatrixXi spins) {
+    int dim = spins.rows();
+    MatrixXi H(dim, dim) ;
+    H = MatrixXi::Zero(dim, dim);
+
+    int y;
+
+    // An welcher Stelle in der Hamiltonmatrix steht der permutierte Vektor
+    // (spin_encoder) ?
+    // An dieser Stelle die Hamiltonmatrix um den entsprechenden Faktor erhoehen
+    // Dafuer jede Permutation von jedem Spinvektor berechnen, um die
+    // entsprechenden Faktoren zu erhalten.
+
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < spins.cols(); j++) {
+            y = spin_encoder(spins, zustandssumme(permutation(spins.row(i), j)));
+            H(i, y) += 2;  // Permutationsteil von $H = sum( 2P - 1 )$
+            H(i, i) -= 1;  // -1 Teil vom Hamiltonian
+        }
+    }
+
+    return H;
+}
+
+int spin_encoder(MatrixXi spinvec, int need_spin) {
+    int dim = spinvec.rows();
+    VectorXi spinzustaende(dim);
+
+    for (int i = 0; i < dim; i++) {
+        spinzustaende(i) = zustandssumme(spinvec.row(i));
+    }
+
+    for (int i = 0; i < dim; i++) {
+        if (spinzustaende(i) == need_spin) {
+            return i;
+        }
+    }
+    return 666;
+}
+
+// Berechne Binardaerstellung des Spins (eindeutige Darstellung)
+int zustandssumme(VectorXi vec) {
+    int zustandssumme = 0;
+    for (int i = 0; i < vec.rows(); i++) {
+        zustandssumme += vec(i) * pow(2, vec.rows() - i - 1);
+    }
+    return zustandssumme;
+}
+
+// Permutiere Eintrag  i mit Eintrag i+1 eines Vektors
+VectorXi permutation(VectorXi vec, int i) {
+    int N = vec.rows();
+    int j = (N + i + 1 ) % N;
+    int temp = vec(i);
+    vec(i) = vec(j);
+    vec(j) = temp;
+    return vec;
+}
+
+// Save Matrix to Filename
+void save_matrices_to_file(MatrixXi mat, const char *filename) {
+    std::ofstream file (filename);
+    file << mat << endl;
 }
