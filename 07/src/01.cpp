71389d526ef173a84b6484645411163518881dd8
Maximilian Sackel <maximilian.sackel@gmx.de>
2018-06-18 19:16:23 +0200

    beaerbeiten git file aufgabe 1

--- 07/src/01.cpp
+++ 07/src/01.cpp
@@ -45,17 +45,28 @@ struct solutions euler(vector<double> t_i, double r_0, double v_0){
         r_i.push_back(
                 r_i[i-1] + stepsize(t_i,i) * pow(omega,2) * v_i[i-1]
                 );
-        // cout << "=========i: " << i << " ==============" <<endl;
-        // cout << "Stepsize: " << t_i[i] << " - " << t_i[i-1] << " = " << stepsize(t_i,i) <<endl;
-        // cout << "t_i: " << t_i[i] <<endl;
-        // cout << "r_i: " << r_i[i] <<endl;
-        // cout << "r_i.back: " << r_i[i-1] <<endl;
-        // cout << "v_i: " << v_i[i] <<endl;
-        // cout << "v_i.back: " << v_i[i-1] <<endl;
     }
     solutions s_euler ;
-    s_euler.r_i = r_i;
-    s_euler.v_i = v_i;
+    s_euler.r_i = r_i; s_euler.v_i = v_i;
+    return s_euler;
+}
+
+struct solutions runge(vector<double> t_i, double r_0, double v_0){
+    vector<double> r_i = {r_0};
+    vector<double> v_i = {v_0};
+
+    double omega = 1.;
+
+    for(int i=1; i<t_i.size(); i++){
+        v_i.push_back(
+                v_i[i-1] - stepsize(t_i,i) * pow(omega,2) * r_i[i-1]
+                );
+        r_i.push_back(
+                r_i[i-1] + stepsize(t_i,i) * pow(omega,2) * v_i[i-1]
+                );
+    }
+    solutions s_euler ;
+    s_euler.r_i = r_i; s_euler.v_i = v_i;
     return s_euler;
 }
 
@@ -63,14 +74,16 @@ int main(int argc, char *argv[])
 {
 
     double t_0 = 0;
-    double T = 16.;
-    int h = 20000;
+    double T = 10.;
+    int h = 20;
 
+    // euler solution
     vector<double> t_i = diskretisierung(t_0, T, h);
     struct solutions s_euler = euler(t_i, 0, 1);
 
+    // save data
     std::ofstream file ("build/euler.txt");
-    
+    file << T << "; " << std::endl;
     for(unsigned i = 0; i< s_euler.r_i.size(); ++i) {
         file << s_euler.r_i[i] << "; " << s_euler.v_i[i] << std::endl;
     }
--- 07/src/plot_1.py
+++ 07/src/plot_1.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 
 s_euler= np.genfromtxt('./build/euler.txt', delimiter=';')
 
-r_i = s_euler[:,0]
+T = s_euler[0,0]
+r_i = s_euler[1:,0]
 v_i = s_euler[:,1]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(range(len(r_i)), r_i, label='r(t)')
-ax.plot(range(len(v_i)), v_i, label='v(t)')
+ax.plot(np.linspace(0,T,len(r_i)), r_i, label='r(t)')
+ax.plot(np.linspace(0,T,len(v_i)), v_i, label='v(t)')
 ax.legend(loc='best')
 fig.savefig('build/euler.pdf')
