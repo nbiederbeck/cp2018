d3ae6a1fb9e590bfb311ef16070f3d1de75584d0
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-06-20 11:50:03 +0200

    Implement euler, runge kutta 2+4, energy

--- 07/src/01.cpp
+++ 07/src/01.cpp
@@ -10,7 +10,8 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-vector<double> diskretisierung(double t_0, double T, int h){
+vector<double> diskretisierung(double t_0, double T, int h)
+{
     vector<double> t = {t_0};
     for(int i=1; i<=h; i++){
         t.push_back(t_0 + T/h * i);
@@ -18,29 +19,38 @@ vector<double> diskretisierung(double t_0, double T, int h){
     return t;
 }
 
-double stepsize(vector<double> t, int i){
+double stepsize(vector<double> t, int i)
+{
     return t[i+1] - t[i];
 }
 
 struct solutions {
     Eigen::MatrixXd r_i;
     Eigen::MatrixXd v_i;
+    Eigen::VectorXd energy;
 };
 
-Eigen::VectorXd func(double t, Eigen::VectorXd x_i){
+Eigen::VectorXd func(double t, Eigen::VectorXd x_i)
+{
     // die funktion haengt explizit nicht von t ab
     double omega = 1;
-
     return pow(omega,2) * x_i;
 }
 
-Eigen::VectorXd force(Eigen::VectorXd r) {
-    double k = 1.;
+Eigen::VectorXd force(Eigen::VectorXd r, double k)
+{
     return -k * r;
 }
 
 
-struct solutions euler(double h, int T, Eigen::VectorXd r_0, Eigen::VectorXd v_0){
+struct solutions euler(
+        double h,  // schrittlaenge
+        int T,  // obere integrationsgrenze
+        Eigen::VectorXd r_0,
+        Eigen::VectorXd v_0,
+        double mass,
+        double k) // lineare rueckstellkraft
+{
     Eigen::MatrixXd r_i = r_0;
     Eigen::MatrixXd v_i = v_0;
 
@@ -52,8 +62,8 @@ struct solutions euler(double h, int T, Eigen::VectorXd r_0, Eigen::VectorXd v_0
         Eigen::MatrixXd joined_v_i(r_i.rows(), r_i.cols()+1);
 
         // physics
-        v_n << v_i.col(i) + h * force(r_i.col(i));
-        r_n << r_i.col(i) + h * v_n;
+        v_n << v_i.col(i) + h * force(r_i.col(i), k) / mass;
+        r_n << r_i.col(i) + h * v_i.col(i);
 
         // fill old var with new values
         joined_r_i << r_i, r_n;
@@ -67,7 +77,8 @@ struct solutions euler(double h, int T, Eigen::VectorXd r_0, Eigen::VectorXd v_0
     return eul;
 }
 
-struct solutions rungekutta_2(double h, int T, Eigen::VectorXd r_0, Eigen::VectorXd v_0) {
+struct solutions rungekutta_2(double h, int T, Eigen::VectorXd r_0, Eigen::VectorXd v_0, double k, double mass)
+{
     Eigen::MatrixXd r_i = r_0;
     Eigen::MatrixXd v_i = v_0;
 
@@ -83,12 +94,12 @@ struct solutions rungekutta_2(double h, int T, Eigen::VectorXd r_0, Eigen::Vecto
 
         // physics
         // v, y_1
-        k1 = h * force(r_i.col(i));
-        k2 = h * force(r_i.col(i) + 0.5 * k1);
+        k1 = h * force(r_i.col(i), k) / mass;
+        k2 = h * force(r_i.col(i) + 0.5 * k1, k) / mass;
         v_n << v_i.col(i) + k2;
         // r, y_0
-        k1 = h * v_n;
-        k2 = h * (v_n + 0.5 * k1);
+        k1 = h * v_i.col(i);
+        k2 = h * (v_i.col(i) + 0.5 * k1);
         r_n << r_i.col(i) + k2;
 
         // fill old var with new values
@@ -103,36 +114,48 @@ struct solutions rungekutta_2(double h, int T, Eigen::VectorXd r_0, Eigen::Vecto
     return rk2;
 }
 
-struct solutions runge_kutta_4(vector<double> t_i, Eigen::VectorXd r_0, Eigen::VectorXd v_0){
+struct solutions rungekutta_4(double h, int T, Eigen::VectorXd r_0, Eigen::VectorXd v_0, double k, double mass)
+{
     Eigen::MatrixXd r_i = r_0;
     Eigen::MatrixXd v_i = v_0;
 
-    int r = r_i.rows();
-    Eigen::VectorXd r_n(r);
-    Eigen::VectorXd v_n(r);
+    Eigen::VectorXd r_n(r_0.rows());
+    Eigen::VectorXd v_n(r_0.rows());
 
-    Eigen::VectorXd k_1(r);
-    Eigen::VectorXd k_2(r);
-    Eigen::VectorXd k_3(r);
-    Eigen::VectorXd k_4(r);
+    Eigen::VectorXd k1(r_0.rows());
+    Eigen::VectorXd k2(r_0.rows());
+    Eigen::VectorXd k3(r_0.rows());
+    Eigen::VectorXd k4(r_0.rows());
 
-    for(int i=1; i<t_i.size(); i++){
+    for(int i = 0; i < T / h; i++){
         // init help var
-        Eigen::MatrixXd joined_r_i(r, i+1);
-        Eigen::MatrixXd joined_v_i(r, i+1);
+        Eigen::MatrixXd joined_r_i(r_i.rows(), r_i.cols()+1);
+        Eigen::MatrixXd joined_v_i(r_i.rows(), r_i.cols()+1);
 
         // physics
-        k_1 << func(t_i[i], v_i.col(i-1));
-        k_2 << func(t_i[i] + 1./2* stepsize(t_i,i), v_i.col(i-1) + stepsize(t_i,i)*1./2*k_1);
-        k_3 << func(t_i[i] + 1./2* stepsize(t_i,i), v_i.col(i-1) + stepsize(t_i,i)*1./2*k_2);
-        k_4 << func(t_i[i] + 2./2* stepsize(t_i,i), v_i.col(i-1) + stepsize(t_i,i)*2./2*k_3);
-        r_n << r_i.col(i-1) - stepsize(t_i,i) * (1./6 * k_1 + 1./3 * k_2 + 1./3 * k_3 + 1./6 * k_4);
+        k1 = h * force(r_i.col(i), k) / mass;
+        k2 = h * force(r_i.col(i) + 0.5 * k1, k) / mass;
+        k3 = h * force(r_i.col(i) + 0.5 * k2, k) / mass;
+        k4 = h * force(r_i.col(i) + 1.0 * k3, k) / mass;
+        v_n << v_i.col(i) + 1./6. * (k1 + 2 * k2 + 2 * k3 + k4);
 
-        k_1 << func(t_i[i], r_i.col(i-1));
-        k_2 << func(t_i[i] + 1./2* stepsize(t_i,i), r_i.col(i-1) +stepsize(t_i,i)*1./2*k_1);
-        k_3 << func(t_i[i] + 1./2* stepsize(t_i,i), r_i.col(i-1) +stepsize(t_i,i)*1./2*k_2);
-        k_4 << func(t_i[i] + 2./2* stepsize(t_i,i), r_i.col(i-1) +stepsize(t_i,i)*2./2*k_3);
-        v_n << v_i.col(i-1) + stepsize(t_i,i) * (1./6 * k_1 + 1./3 * k_2 + 1./3 * k_3 + 1./6 * k_4);
+        k1 = h * v_i.col(i);
+        k2 = h * (v_i.col(i) + 0.5 * k1);
+        k3 = h * (v_i.col(i) + 0.5 * k2);
+        k4 = h * (v_i.col(i) + 1.0 * k3);
+        r_n << r_i.col(i) + 1./6. * (k1 + 2 * k2 + 2 * k3 + k4);
+
+        // k_1 << func(t_i[i], v_i.col(i-1));
+        // k_2 << func(t_i[i] + 1./2* stepsize(t_i,i), v_i.col(i-1) + stepsize(t_i,i)*1./2*k_1);
+        // k_3 << func(t_i[i] + 1./2* stepsize(t_i,i), v_i.col(i-1) + stepsize(t_i,i)*1./2*k_2);
+        // k_4 << func(t_i[i] + 2./2* stepsize(t_i,i), v_i.col(i-1) + stepsize(t_i,i)*2./2*k_3);
+        // r_n << r_i.col(i-1) - stepsize(t_i,i) * (1./6 * k_1 + 1./3 * k_2 + 1./3 * k_3 + 1./6 * k_4);
+
+        // k_1 << func(t_i[i], r_i.col(i-1));
+        // k_2 << func(t_i[i] + 1./2* stepsize(t_i,i), r_i.col(i-1) +stepsize(t_i,i)*1./2*k_1);
+        // k_3 << func(t_i[i] + 1./2* stepsize(t_i,i), r_i.col(i-1) +stepsize(t_i,i)*1./2*k_2);
+        // k_4 << func(t_i[i] + 2./2* stepsize(t_i,i), r_i.col(i-1) +stepsize(t_i,i)*2./2*k_3);
+        // v_n << v_i.col(i-1) + stepsize(t_i,i) * (1./6 * k_1 + 1./3 * k_2 + 1./3 * k_3 + 1./6 * k_4);
 
         // fill old var with new values
         joined_r_i << r_i, r_n;
@@ -145,7 +168,22 @@ struct solutions runge_kutta_4(vector<double> t_i, Eigen::VectorXd r_0, Eigen::V
     return s_euler;
 }
 
-void save(struct solutions sol, double T, const std::string &filename){
+struct solutions energy(struct solutions sol, double m, double k) {
+    Eigen::VectorXd energy(sol.r_i.cols());
+
+    for (int i = 0; i < sol.r_i.cols(); i++) {
+        energy(i) =
+            m / 2. * sol.v_i.col(i).squaredNorm()  // kinetische Energie
+            + k / 2. * sol.r_i.col(i).squaredNorm();  // potentielle Energie
+    }
+
+    sol.energy = energy;
+
+    return sol;
+}
+
+void save(struct solutions sol, double T, const std::string &filename)
+{
     std::string path = "build/"+ filename + "/";
     std::system(("mkdir -p "+path).c_str());
     std::ofstream file;
@@ -158,34 +196,45 @@ void save(struct solutions sol, double T, const std::string &filename){
     file << sol.r_i << std::endl;
     file.close();
 
-
     file.open(path+"v_i.txt");
     file << sol.v_i << std::endl;
+    file.close();
+
+    file.open(path+"energy.txt");
+    file << sol.energy << std::endl;
     file.close();
 }
 
 
-int main(int argc, char *argv[])
+int main()
 {
 
     // setze Start/Rand-bedingugen
     double t_0 = 0;
     double T = 20. * M_PI;
-    double h = M_PI / 100.;
+    double h = M_PI / 10.;
     const int dim = 3;
+    double mass = 1.;
+    double k = 1.;
 
     // initaliziere Startvektoren
     Eigen::VectorXd r_0(dim);
     Eigen::VectorXd v_0(dim);
-    r_0 << 1, 0, 0;
-    v_0 << 0, 0, 0;
+    r_0 << 0, 0, 0;
+    v_0 << 1, 1, 1;
 
-    // euler solution
-    struct solutions s_euler = euler(h, T, r_0, v_0);
+    struct solutions s_euler = euler(h, T, r_0, v_0, mass, k);
+    s_euler = energy(s_euler, mass, k);
     save(s_euler, T, "euler");
 
-    struct solutions s_runge = rungekutta_2(h, T, r_0, v_0);
-    save(s_runge, T, "runge");
+    struct solutions s_runge2 = rungekutta_2(h, T, r_0, v_0, mass, k);
+    s_runge2 = energy(s_runge2, mass, k);
+    save(s_runge2, T, "runge2");
+
+    struct solutions s_runge4 = rungekutta_4(h, T, r_0, v_0, mass, k);
+    s_runge4 = energy(s_runge4, mass, k);
+    save(s_runge4, T, "runge4");
+
 
     return 0;
 }
--- 07/src/plot_1.py
+++ 07/src/plot_1.py
@@ -1,33 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class plotter:
     def __init__(self, path):
         # data = np.genfromtxt(path, delimiter=';')
-        self.T = np.genfromtxt(path+'T.txt')
-        self.r_i = np.genfromtxt(path+'r_i.txt')
-        self.v_i = np.genfromtxt(path+'v_i.txt')
+        self.T = np.genfromtxt(path + "T.txt")
+        self.r_i = np.genfromtxt(path + "r_i.txt")
+        self.v_i = np.genfromtxt(path + "v_i.txt")
+        self.energy = np.genfromtxt(path + "energy.txt")
         # self.r_i = data[1:,0]
         # self.v_i = data[:,1]
 
-    def make_plot(self, path, title, dim):
+    def make_plot(self, path, title, dim, lim=None):
         fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(np.linspace(0,self.T,len(self.r_i[dim,:])), self.r_i[dim,:], label='r(t)')
-        ax.plot(np.linspace(0,self.T,len(self.v_i[dim,:])), self.v_i[dim,:], label='v(t)')
-        ax.set_xlabel('time')
-        ax.set_ylabel('Amplitude')
-        ax.legend(loc='best')
+        ax = fig.add_subplot(211)
+        ax.plot(
+            np.linspace(0, self.T, len(self.r_i[dim, :])),
+            self.r_i[dim, :],
+            label="r(t)",
+        )
+        ax.plot(
+            np.linspace(0, self.T, len(self.v_i[dim, :])),
+            self.v_i[dim, :],
+            label="v(t)",
+        )
+        ax.set_xlabel("time")
+        ax.set_ylabel("Amplitude")
+
+        # ax.set_xlim([0, 2 * np.pi])
+        # ax.set_ylim([-1.5, 1.5])
+        if lim is None:
+            lim = 1.2 * np.max(np.abs([self.v_i, self.r_i]))
+        ax.set_ylim([-lim, lim])
+
+        ax.legend(loc="best")
         ax.set_title(title)
         ax.grid()
-        fig.tight_layout(pad=0)
+
+        ax = fig.add_subplot(212)
+        ax.plot(np.linspace(0, self.T, len(self.energy)), self.energy, label="Energie")
+        ax.legend()
+        ax.set_ylim([0, 1.2 * np.max(self.energy)])
+
+        fig.tight_layout(pad=0.1)
         fig.savefig(path)
 
-def main():
-    p_euler = plotter('build/euler/')
-    p_euler.make_plot('build/euler.png', 'Euler Verfahren', 0)
-    p_runge= plotter('build/runge/')
-    p_runge.make_plot('build/runge.png', 'Runge Verfahren', 0)
+        return lim
 
-if __name__ == '__main__':
+def main():
+    p_euler = plotter("build/euler/")
+    lim = p_euler.make_plot("build/euler.png", "Euler Verfahren", 0)
+    p_runge = plotter("build/runge2/")
+    p_runge.make_plot("build/runge2.png", "Runge Verfahren 2. Ordnung", 0, lim)
+    p_runge = plotter("build/runge4/")
+    p_runge.make_plot("build/runge4.png", "Runge Verfahren 4. Ordnung", 0, lim)
+
+
+if __name__ == "__main__":
     main()
