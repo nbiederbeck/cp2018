33c8c83669641ecd8664bf3626263524cf3a9dd5
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-06-27 15:26:12 +0200

    aufgabe 1 fertig, nur noch texen

--- 08/src/01.cpp
+++ 08/src/01.cpp
@@ -33,41 +33,104 @@ Eigen::Vector4d euler(Eigen::Vector4d state, double h)
     return next;
 }
 
-// double phi_1(vector<double> y){
-//   double phi1;
+Eigen::Vector4d f(
+    Eigen::Vector4d state,
+    double m1=1.0,
+    double m2=1.0,
+    double L1=1.0,
+    double L2=1.0,
+    double g=9.81)
+{
+    Eigen::Vector4d temp;
 
-//   double Vorfaktor = mu * cos(y[1] - y[0]) * cos(y[1] - y[0]);
-//   Vorfaktor = 1 - Vorfaktor;
-//   Vorfaktor = 1 / Vorfaktor;
 
-//   double f1 = mu * g1 * sin(y[1]) * cos(y[1] - y[0]);
-//   double f2 = mu * y[2] * y[2] * sin(y[1] - y[0]) * cos(y[1] - y[0]);
-//   double f3 = -g1 * sin(y[0]);
-//   double f4 = (mu / lambda) * y[3] * y[3] * sin(y[1] - y[0]);
+    temp(0) = state(2);  // Erster Eintrag ist Este Ableitung, also dritter Eintrag
+    temp(1) = state(3);
+    // Dritter Eintrag ist zweite Ableitung
 
-//   phi1 = Vorfaktor * (f1 + f2 + f3 + f4);
-//   return phi1;
-// }
+    double mu = m2 / (m1 + m2);
+    double lambda = L1 / L2;
+    double g1 = g / L1;
+    double g2 = g / L2;
+    // Kurzschreibweisen
+    double t1 = state(0);
+    double t2 = state(1);
+    double sin_t1 = std::sin(t1);
+    double sin_t2 = std::sin(t2);
+    double cos_t1 = std::cos(t1);
+    double cos_t2 = std::cos(t2);
+    double sin_t2_t1 = std::sin(t2 - t1);
+    double cos_t2_t1 = std::cos(t2 - t1);
 
-// double phi_2(vector<double> y){
-//   double phi2;
+    double f = 1.0 / (1.0 - mu * std::pow(cos_t2_t1, 2));  // Vorfaktor, bei beiden gleich
 
-//   double Vorfaktor = mu * cos(y[1] - y[0]) * cos(y[1] - y[0]);
-//   Vorfaktor = 1 - Vorfaktor;
-//   Vorfaktor = 1 / Vorfaktor;
+    temp(2) = f * (
+              mu * g1 * sin_t2 * cos_t2_t1
+              + mu * std::pow(state(2), 2) * sin_t2_t1 * cos_t2_t1
+              - g1 * sin_t1
+              + (mu / lambda) * std::pow(state(3), 2) * sin_t2_t1
+              );
+    temp(3) = f * (
+              g2 * sin_t1 * cos_t2_t1
+              - mu * std::pow(state(3), 2) * sin_t2_t1 * cos_t2_t1
+              - g2 * sin_t2
+              - mu * std::pow(state(2), 2) * sin_t2_t1
+              );
 
-//   double f1 = g2 * sin(y[0]) * cos(y[1] - y[0]);
-//   double f2 = -mu * y[3] * y[3] * sin(y[1] - y[0]) * cos(y[1] - y[0]);
-//   double f3 = -g2 * sin(y[1]);
-//   double f4 = -lambda * y[2] * y[2] * sin(y[1] - y[0]);
+    return temp;
+}
 
-//   phi2 = Vorfaktor * (f1 + f2 + f3 + f4);
-//   return phi2;
-// }
+Eigen::Vector4d rungekutta(
+    Eigen::Vector4d state,
+    double h)
+{
+    Eigen::Vector4d k1 = h * f(state);
+    Eigen::Vector4d k2 = h * f(state + 0.5 * k1);
+    Eigen::Vector4d k3 = h * f(state + 0.5 * k2);
+    Eigen::Vector4d k4 = h * f(state + k3);
+
+    return state + k4;
+}
+
+double Epot(
+    Eigen::Vector4d state,
+    double m1=1.0,
+    double m2=1.0,
+    double L1=1.0,
+    double L2=1.0,
+    double g=9.81)
+{
+    // Umschreiben in orthogonale Koordinaten
+    // double x1 = L1 * std::sin(state(0));  // theta_1
+    double y1 = L1 * std::cos(state(0));  // theta_1
+    // double x2 = x1 + L2 * std::sin(state(1));  // theta_2
+    double y2 = y1 + L2 * std::cos(state(1));  // theta_2
+
+    double E = m1 * g * (L1 - y1) + m2 * g * (L1 + L2 - y2);
+    return E;
+}
+
+double Ekin(
+    Eigen::Vector4d state,
+    double m1=1.0,
+    double m2=1.0,
+    double L1=1.0,
+    double L2=1.0,
+    double g=9.81)
+{
+    // Umschreiben in orthogonale Koordinaten
+    double x1 = L1 * std::cos(state(0)) * state(2);  // theta_1
+    double y1 = L1 * std::sin(state(0)) * state(2);  // theta_1
+    double x2 = x1 + L2 * std::cos(state(1)) * state(3);  // theta_2
+    double y2 = y1 + L2 * std::sin(state(1)) * state(3);  // theta_2
+
+    double E = m1 / 2.0 * (x1 * x1 + y1 * y1) + m2 / 2.0 * (x2 * x2 + y2 * y2);
+    return E;
+}
 
 void b()
 {
-    cout << "b) Testen, ob System in der Naeherung schwingt." << endl;
+    cout << "b) Testen, ob System in der Naeherung schwingt (Euler Verfahren)." << endl;
 
     Eigen::Vector4d init = initialize(false);
     Eigen::Vector4d state = init;
@@ -78,7 +141,52 @@ void b()
     for (int i; i < 100000; ++i) {
         state = euler(state, 0.001);
 
-        pendulum_data << state(0) << " " << state(1) << endl;
+        // cout << state(0) << " " << state(1) << endl;
+        pendulum_data
+            << state(0) << " "
+            << state(1) << " "
+            << state(2) << " "
+            << state(3) << " "
+            << endl;
+    }
+
+    cout << "Done" << endl << endl;
+}
+
+void c(double h)
+{
+    cout << "c) Schwingungen mit Runge-Kutta 4. Ordnung." << endl;
+
+    // Vorzeichen +
+    Eigen::Vector4d state_plus = initialize(false);
+    std::ofstream pendulum_data_plus;
+    pendulum_data_plus.open("build/pendulum_data_c_plus.txt");
+
+    // Vorzeichen -
+    Eigen::Vector4d state_minus = initialize(true);
+    std::ofstream pendulum_data_minus;
+    pendulum_data_minus.open("build/pendulum_data_c_minus.txt");
+
+    for (int i; i < 100000; ++i) {
+        state_plus = rungekutta(state_plus, h);
+        pendulum_data_plus
+            << state_plus(0) << " "     // theta 1
+            << state_plus(1) << " "     // theta 2
+            << state_plus(2) << " "     // theta 1 punkt
+            << state_plus(3) << " "     // theta 2 punkt
+            << Epot(state_plus) << " "  // potentielle Energie
+            << Ekin(state_plus) << " "  // kinetische Energie
+            << endl;
+
+        state_minus = rungekutta(state_minus, h);
+        pendulum_data_minus
+            << state_minus(0) << " "     // theta 1
+            << state_minus(1) << " "     // theta 2
+            << state_minus(2) << " "     // theta 1 punkt
+            << state_minus(3) << " "     // theta 2 punkt
+            << Epot(state_minus) << " "  // potentielle Energie
+            << Ekin(state_minus) << " "  // kinetische Energie
+            << endl;
     }
 
     cout << "Done" << endl << endl;
@@ -87,5 +195,6 @@ void b()
 int main()
 {
     b();
+    c(0.0001);
     return 0;
 }
--- 08/src/plot_pendulum_c.py
+++ 08/src/plot_pendulum_c.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_theta_time():
-    theta1, theta2, theta_1, theta_2 = np.genfromtxt("build/pendulum_data_b.txt", unpack=True)
+def plot_theta_time(sign):
+    theta1, theta2, theta_1, theta_2, epot, ekin = np.genfromtxt("build/pendulum_data_c_{}.txt".format(sign), unpack=True)
     time = range(len(theta1))
 
-    fig, (ax1, ax2) = plt.subplots(nrows=2)
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3)
 
     ax1.plot(time, theta1, label=r"$\theta_1$")
     ax1.plot(time, theta2, label=r"$\theta_2$")
@@ -20,12 +20,19 @@ def plot_theta_time():
     ax2.set_ylabel(r"$A(t)$")
     ax2.legend()
 
+    ax3.plot(time, epot, label=r"$E_{pot}$")
+    ax3.plot(time, ekin, label=r"$E_{kin}$")
+    ax3.plot(time, ekin + epot, label=r"$\sum E$")
+    ax3.set_xlabel(r"$t$")
+    ax3.set_ylabel(r"$E$")
+    ax3.legend()
+
     fig.tight_layout()
-    fig.savefig("build/pendulum_b.png")
+    fig.savefig("build/pendulum_c_{}.png".format(sign))
 
 
-def plot_xy():
-    theta1, theta2, theta_1, theta_2 = np.genfromtxt("build/pendulum_data_b.txt", unpack=True)
+def plot_xy(sign):
+    theta1, theta2, theta_1, theta_2, epot, ekin = np.genfromtxt("build/pendulum_data_c_{}.txt".format(sign), unpack=True)
 
     x1, y1 = np.sin(theta1), - np.cos(theta1)
     x2, y2 = x1 + np.sin(theta2), y1 - np.cos(theta2)
@@ -42,12 +49,14 @@ def plot_xy():
     ax.set_xlim([-1, 1])
     ax.set_aspect(1)
     fig.tight_layout()
-    fig.savefig("build/pendulum_xy_b.png")
+    fig.savefig("build/pendulum_xy_c_{}.png".format(sign))
 
 
 def main():
-    plot_theta_time()
-    plot_xy()
+    plot_theta_time("plus")
+    plot_xy("plus")
+    plot_theta_time("minus")
+    plot_xy("minus")
 
 
 if __name__ == "__main__":
