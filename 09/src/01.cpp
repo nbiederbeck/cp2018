76508cd8a5d8f03bde30c37ca51679b991bfda64
Maximilian Sackel <maximilian.sackel@gmx.de>
2018-07-04 10:50:07 +0200

    add dir 09

---
+++ 09/src/01.cpp
@@ -0,0 +1,13 @@
+// vim: commentstring=//%s expandtab shiftwidth=4
+#include <Eigen/Dense>
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
